27ff2ba8e1b32f6722c58b036ffb84b501d1523d
KATAMII <lydiamwaah01@gmail.com>
2024-06-05 12:26:43 +0300

    set up the project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './components/Header'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <Header/>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../components/Header.css'
+
+function Header() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Header
