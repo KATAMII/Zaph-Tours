6d44a1166e853fe33dd7bdc98e244ec362911894
KATAMII <lydiamwaah01@gmail.com>
2024-06-08 00:32:35 +0300

    finish Trips page

--- src/Pages/Trips/Trips.jsx
+++ src/Pages/Trips/Trips.jsx
@@ -25,6 +25,34 @@ import wildlife2 from '../../assets/wildlife2.jpeg';
 import wildlife3 from '../../assets/wildlife3.jpeg';
 import wildlife4 from '../../assets/wildlife4.jpeg';
 
+import eco1 from '../../assets/eco1.jpeg';
+import eco2 from '../../assets/eco2.jpeg';
+import eco3 from '../../assets/eco3.jpeg';
+import eco4 from '../../assets/eco4.jpeg';
+
+import luxury1 from '../../assets/luxury1.jpeg';
+import luxury2 from '../../assets/luxury2.jpeg';
+import luxury3 from '../../assets/luxury3.jpeg';
+import luxury4 from '../../assets/luxury4.jpeg';
+
+import beach1 from '../../assets/beach1.jpeg';
+import beach2 from '../../assets/beach2.jpeg';
+import beach3 from '../../assets/beach3.jpeg';
+import beach4 from '../../assets/beach4.jpeg';
+
+import well1 from '../../assets/well1.jpeg';
+import well2 from '../../assets/well2.jpeg';
+import well3 from '../../assets/well3.jpeg';
+import well4 from '../../assets/well4.jpeg';
+
+import culinary1 from '../../assets/culinary1.jpeg';
+import culinary2 from '../../assets/culinary2.jpeg';
+import culinary3 from '../../assets/culinary3.jpeg';
+import culinary4 from '../../assets/culinary4.jpeg';
+
+
+
+
 
 function Trips() {
   const packages = [
@@ -59,10 +87,34 @@ function Trips() {
       pricing: 'Starting from $1500 per family'
     },
     {
-      title: 'Wildlife safaris',
-      images: [wildlife1, wildlife2, wildlife3, wildlife4],
-      description: 'Enjoy a fun-filled wildlife safaris with activities for all ages.',
+      title: 'eco-tours',
+      images: [eco1, eco2, eco3, eco4],
+      description: 'Enjoy a fun-filled eco-activities with activities for all ages.',
       pricing: 'Starting from $1500 per family'
+    },
+    {
+      title: 'Luxary Vacations',
+      images: [luxury1, luxury2, luxury3, luxury4],
+      description: 'Enjoy a fun-filled eco-activities with activities for all ages.',
+      pricing: 'Starting from $1500 per family'
+    },
+    {
+      title: 'Beach Holidays',
+      images: [beach1, beach2, beach3, beach4],
+      description: 'Enjoy a fun-filled holidays Beach holid with activities for all ages.',
+      pricing: 'Starting from $2000 per family'
+    },
+    {
+      title: 'Wellness retreats',
+      images: [well1, well2, well3, well4],
+      description: 'Enjoy a fun-filled welness retreats with activities for all ages.',
+      pricing: 'Starting from $2000 per family'
+    },
+    {
+      title: 'Culinary Tours',
+      images: [culinary1, culinary2, culinary3, culinary4],
+      description: 'Enjoy a fun-filled Culinary Tours with activities for all ages.',
+      pricing: 'Starting from $2000 per family'
     },
    
   ];
